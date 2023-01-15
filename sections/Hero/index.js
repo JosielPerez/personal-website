da222b9038f6a3c29e21a5e3a31f4ea1f0ce7b95
Josiel Perez <josielp0218@gmail.com>
2023-01-15 02:17:28 -0500

    hero page styles and html

---
+++ sections/Hero/index.js
@@ -0,0 +1,17 @@
+import styles from "@/styles/Hero.module.css";
+export default function Hero() {
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.top_section}>
+          <h1 className={styles.brand_name}>Josiel</h1>
+        </div>
+        <div className={styles.bottom_section}>
+          <h2 className={styles.sub_title}>
+            // Software Engineer && Full Stack Developer
+          </h2>
+        </div>
+      </div>
+    </>
+  );
+}
