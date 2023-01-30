bc19d55f55b7157cb48bffc0246bdc13f2a6df6a
Josiel Perez <josielp0218@gmail.com>
2023-01-30 18:39:52 -0500

    updated new resume

--- sections/Contact/index.jsx
+++ sections/Contact/index.jsx
@@ -1,39 +1,65 @@
 import styles from "@/styles/Contact.module.css";
+import emailjs from "@emailjs/browser";
 
 export default function Hero() {
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const message = event.target.message.value;
+    const data = {
+      your_name: name,
+      your_email: email,
+      message: message,
+    };
+
+    const response = await emailjs.sendForm(
+      `${process.env.YOUR_SERVICE_ID}`,
+      `${process.env.YOUR_TEMPLATE_ID}`,
+      event.target,
+      `${process.env.YOUR_PUBLIC_KEY}`
+    );
+  }
+
   return (
     <>
       <div className={styles.container} id="contact">
         <div className={styles.top_section}>
           <h1>Send me a Message!</h1>
         </div>
-        <div className={styles.bottom_section}>
+        <form className={styles.bottom_section} onSubmit={handleSubmit}>
           <div className={styles.top_row}>
             <div className={styles.column}>
               <label className={styles.name}>Your Name</label>
               <input
+                name="name"
                 placeholder="Enter Your Name"
                 className={styles.input}
               ></input>
             </div>
             <div className={styles.column}>
               <label className={styles.name}>Your Email</label>
-              <input placeholder="Enter Your Email" className={styles.input} />
+              <input
+                name="email"
+                placeholder="Enter Your Email"
+                className={styles.input}
+              />
             </div>
           </div>
           <div className={styles.bottom_row}>
             <div className={styles.column} style={{ width: "100%" }}>
               <label className={styles.name}>Your Message</label>
               <input
+                name="message"
                 placeholder="Enter Your Message"
                 className={styles.input}
               />
             </div>
           </div>
           <div className={styles.button_row}>
-            <button className={styles.submit}>Submit</button>
+            <input type="submit" value="Submit" className={styles.submit} />
           </div>
-        </div>
+        </form>
       </div>
     </>
   );
