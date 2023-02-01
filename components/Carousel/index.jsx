5c0b28d41c61831f929eef3dc74b9f18c6deb55c
Josiel Perez <josielp0218@gmail.com>
2023-02-01 01:02:22 -0500

    created a functional carousel for project section

---
+++ components/Carousel/index.jsx
@@ -0,0 +1,47 @@
+import styles from "./styles.module.css";
+import { useState } from "react";
+import { BsArrowRightCircle } from "react-icons/bs";
+
+export default function Carousel(props) {
+  const [xPos, setXPos] = useState(0);
+  const LastCardPosition = 2850;
+
+  function handleRightClick() {
+    if (xPos * -1 >= LastCardPosition) {
+      setXPos(0);
+      return;
+    }
+    setXPos(xPos - 950);
+  }
+
+  function handleLeftClick() {
+    if (xPos * -1 <= 0) {
+      setXPos(-LastCardPosition);
+      return;
+    }
+    setXPos(xPos + 950);
+  }
+
+  return (
+    <>
+      <div className={styles.wrapper}>
+        <BsArrowRightCircle
+          onClick={handleRightClick}
+          className={styles.right_button}
+        />
+        <BsArrowRightCircle
+          onClick={handleLeftClick}
+          className={styles.left_button}
+        />
+        <div className={styles.container}>
+          <div
+            style={{ transform: `translate(${xPos}px)` }}
+            className={styles.bar}
+          >
+            {props.children}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- sections/Projects/index.js
+++ sections/Projects/index.js
@@ -2,17 +2,32 @@ import styles from "@/styles/Projects.module.css";
 import ProjectCard from "@/components/ProjectCard";
 import { DiPython } from "react-icons/di";
 import { SiCplusplus } from "react-icons/si";
+import Carousel from "@/components/Carousel";
 
-export default function Hero() {
+export default function Projects() {
   return (
     <>
       <div className={styles.container} id="projects">
         <div className={styles.top_section}></div>
         <div className={styles.bottom_section}>
-          <ProjectCard link="https://github.com/JosielPerez/personal-website">
-            <DiPython style={{ width: "30px", height: "30px" }} />
-            <SiCplusplus style={{ width: "30px", height: "30px" }} />
-          </ProjectCard>
+          <Carousel>
+            <ProjectCard link="https://github.com/JosielPerez/personal-website">
+              <DiPython style={{ width: "30px", height: "30px" }} />
+              <SiCplusplus style={{ width: "30px", height: "30px" }} />
+            </ProjectCard>
+            <ProjectCard link="https://github.com/JosielPerez/personal-website">
+              <DiPython style={{ width: "30px", height: "30px" }} />
+              <SiCplusplus style={{ width: "30px", height: "30px" }} />
+            </ProjectCard>
+            <ProjectCard link="https://github.com/JosielPerez/personal-website">
+              <DiPython style={{ width: "30px", height: "30px" }} />
+              <SiCplusplus style={{ width: "30px", height: "30px" }} />
+            </ProjectCard>
+            <ProjectCard link="https://github.com/JosielPerez/personal-website">
+              <DiPython style={{ width: "30px", height: "30px" }} />
+              <SiCplusplus style={{ width: "30px", height: "30px" }} />
+            </ProjectCard>
+          </Carousel>
         </div>
       </div>
     </>
