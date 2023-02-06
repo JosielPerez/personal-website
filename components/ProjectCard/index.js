c784667cc8226df6af1143ceb2a36b2c5b093a8c
Josiel Perez <josielp0218@gmail.com>
2023-02-06 00:35:56 -0500

    added first project to project card

--- components/ProjectCard/index.js
+++ components/ProjectCard/index.js
@@ -6,20 +6,20 @@ function ProjectCard(props) {
       <div className={styles.left_content}>
         <img
           className={styles.project_img}
-          src="/project1.jpg"
+          src="/project1.png"
           alt="project 1"
         />
       </div>
       <div className={styles.right_content}>
-        <h2 className={styles.title}>Project Name</h2>
+        <h2 className={styles.title}>Personal Website</h2>
         <p className={styles.text}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
-          turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec
-          fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus
-          elit sed risus. Maecenas eget condimentum velit, sit amet feugiat
-          lectus. Class aptent taciti sociosqu ad litora torquent per conubia
-          nostra, per inceptos himenaeos. Praesent auctor purus luctus enim
-          egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex.
+          Developed a dynamic and user-friendly personal website that showcases
+          my portfolio. Utilizing the latest web technologies like HTML, CSS,
+          JavaScript and React, the website presents a professional platform for
+          potential employers or clients to view and interact with my projects.
+          The website provides an easy-to-use interface and an engaging user
+          experience to showcase my skills and accomplishments in a modern and
+          professional way
         </p>
         <div className={styles.skill_container}>{props.children}</div>
         <div className={styles.button_container}>
--- sections/Projects/index.js
+++ sections/Projects/index.js
@@ -2,6 +2,9 @@ import styles from "@/styles/Projects.module.css";
 import ProjectCard from "@/components/ProjectCard";
 import { DiPython } from "react-icons/di";
 import { SiCplusplus } from "react-icons/si";
+import { DiJavascript } from "react-icons/di";
+import { DiCss3 } from "react-icons/di";
+import { DiReact } from "react-icons/di";
 import Carousel from "@/components/Carousel";
 
 export default function Projects() {
@@ -12,12 +15,12 @@ export default function Projects() {
         <div className={styles.bottom_section}>
           <Carousel>
             <ProjectCard link="https://github.com/JosielPerez/personal-website">
-              <DiPython style={{ width: "30px", height: "30px" }} />
-              <SiCplusplus style={{ width: "30px", height: "30px" }} />
+              <DiJavascript style={{ width: "30px", height: "30px" }} />
+              <DiCss3 style={{ width: "30px", height: "30px" }} />
+              <DiReact style={{ width: "30px", height: "30px" }} />
             </ProjectCard>
-            <ProjectCard link="https://github.com/JosielPerez/personal-website">
+            <ProjectCard link="https://github.com/JosielPerez/nftfloortracker">
               <DiPython style={{ width: "30px", height: "30px" }} />
-              <SiCplusplus style={{ width: "30px", height: "30px" }} />
             </ProjectCard>
             <ProjectCard link="https://github.com/JosielPerez/personal-website">
               <DiPython style={{ width: "30px", height: "30px" }} />
