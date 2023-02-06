7d6fbfe52eefc5f7bf9825105017078be5dd2ee6
Josiel Perez <josielp0218@gmail.com>
2023-02-05 23:48:28 -0500

    debugging status bar for apple

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,10 @@ export default function Home() {
         <meta name="description" content="Josiel's Personal Website" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/whitelogo.png" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
       </Head>
       <main>
         <TopNav />
